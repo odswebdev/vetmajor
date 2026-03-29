2a892e85c627b4807c33fc63789914e4958f3808
Dmitry Orlov <ods90@mail.ru>
2026-03-30 00:37:16 +0400

    Updates

--- gulp/tasks/swiper-css.js
+++
@@ -1,8 +0,0 @@
-import gulp from "gulp";
-import { paths } from "../config/paths.js";
-
-export const swiperCss = () => {
-  return gulp
-    .src("node_modules/swiper/swiper-bundle.css")
-    .pipe(gulp.dest(paths.buildCssFolder));
-};
