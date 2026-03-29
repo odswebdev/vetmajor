025971848fbfd8d1975a943fd8c4db3b06fd42cd
Dmitry Orlov <ods90@mail.ru>
2026-03-30 00:12:55 +0400

    fix package.json

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -2,5 +2,12 @@ import gulp from "gulp";
 import ghPages from "gulp-gh-pages";
 
 export const deploy = () => {
-  return gulp.src(`${app.paths.base.build}/**/*`).pipe(ghPages());
+  return gulp
+    .src("./dist/**/*")
+    .pipe(
+      ghPages({
+        force: true,
+        message: "deploy " + new Date().toISOString(),
+      })
+    );
 };
